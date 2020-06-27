09fd85f797cb4e4a98a476913301bf13c0e2274e
Sourov <thasan1020@gmail.com>
2020-06-27 21:42:27 +0600

    backendn update

--- app/src/main/java/com/example/myweatherapp/Search.java
+++ app/src/main/java/com/example/myweatherapp/Search.java
@@ -1,14 +1,18 @@
 package com.example.myweatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Search extends AppCompatActivity {
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        recyclerView=(RecyclerView) findViewById(R.id.recView);
     }
 }
